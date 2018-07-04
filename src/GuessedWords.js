fd971c0a719862da8e1428c660d03e3287a1a4d7
Elton Nascimento <eltongarbin@gmail.com>
2018-07-04 20:42:14 -0300

    Tests and code for non-zero guessed words

--- src/GuessedWords.js
+++ src/GuessedWords.js
@@ -7,6 +7,30 @@ const GuessedWords = ({ guessedWords }) => {
     contents = (
       <span data-test="guess-instructions">Try to guess the secret word</span>
     );
+  } else {
+    const guessedWordsRows = guessedWords.map(
+      ({ guessedWord, letterMatchCount }, index) => (
+        <tr data-test="guessed-word" key={index}>
+          <td>{guessedWord}</td>
+          <td>{letterMatchCount}</td>
+        </tr>
+      )
+    );
+
+    contents = (
+      <div data-test="guessed-words">
+        <h3>Guessed Words</h3>
+        <table>
+          <thead>
+            <tr>
+              <th>Guess</th>
+              <th>Matching Letters</th>
+            </tr>
+          </thead>
+          <tbody>{guessedWordsRows}</tbody>
+        </table>
+      </div>
+    );
   }
 
   return <div data-test="component-guessed-words">{contents}</div>;
