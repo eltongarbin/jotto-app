b1db0f4d51e647a19a81014d4fb5131f12905421
Elton Nascimento <eltongarbin@gmail.com>
2018-07-03 21:33:52 -0300

    Add proptypes for Congrats component

--- src/Congrats.js
+++ src/Congrats.js
@@ -3,10 +3,21 @@ import React from 'react';
 /**
  * Functional react component for congratualatory message.
  * @function
+ * @param {object} props - React props.
  * @returns {JSX.Element} - Rendered component (or null if `success` prop is false).
  */
-const Congrats = () => {
-  return <div />;
+const Congrats = (props) => {
+  if (props.success) {
+    return (
+      <div data-test="component-congrats">
+        <span data-test="congrats-message">
+          Congratulations! You guessed the word!
+        </span>
+      </div>
+    );
+  } else {
+    return <div data-test="component-congrats" />;
+  }
 };
 
 export default Congrats;
--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
-import { findByTestAttr } from '../test/testUtils';
+import { findByTestAttr, checkProps } from '../test/testUtils';
 import Congrats from './Congrats';
+
+const defaultProps = { success: false };
 
 /**
  * Factory function to create a ShallowWrapper for the Congrats component.
@@ -11,7 +13,8 @@ import Congrats from './Congrats';
  * @returns {ShallowWrapper}
  */
 const setup = (props = {}) => {
-  return shallow(<Congrats {...props} />);
+  const setupProps = { ...defaultProps, ...props };
+  return shallow(<Congrats {...setupProps} />);
 };
 
 it('renders without error', () => {
@@ -30,4 +33,9 @@ it('renders non-empty congrats message when `success` prop is true', () => {
   const wrapper = setup({ success: true });
   const message = findByTestAttr(wrapper, 'congrats-message');
   expect(message.text().length).not.toBe(0);
+});
+
+it('does not throw warning with expected props', () => {
+  const expectedProps = { success: false };
+  checkProps(Congrats, expectedProps);
 });
