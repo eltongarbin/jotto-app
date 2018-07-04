d2290bf8adfd8b130c523b3f05caa901939774e1
Elton Nascimento <eltongarbin@gmail.com>
2018-07-03 21:09:46 -0300

    Applying TDD for Congrats component

---
+++ src/Congrats.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+/**
+ * Functional react component for congratualatory message.
+ * @function
+ * @returns {JSX.Element} - Rendered component (or null if `success` prop is false).
+ */
+const Congrats = () => {
+  return <div />;
+};
+
+export default Congrats;
