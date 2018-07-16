012f62f30897e399e6704d7ceac6368679bf0b57
Elton Nascimento <eltongarbin@gmail.com>
2018-07-16 20:48:59 -0300

    Add code and tests to clear input box on submit

--- src/Input.js
+++ src/Input.js
@@ -17,6 +17,8 @@ export class Input extends Component {
     if (guessedWord && guessedWord.length > 0) {
       this.props.guessWord(guessedWord);
     }
+
+    this.inputBox.current.value = '';
   }
 
   render() {
